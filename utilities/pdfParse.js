f20f35b8c4d010a33397f4ab90649c2c52cc173c
Zach <zacharyamassey@gmail.com>
2025-09-12 14:39:05 -0400

    added filewrite for pdf parsing sanity check

--- utilities/pdfParse.js
+++ utilities/pdfParse.js
@@ -1,22 +1,38 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
-const splitIntoChunks  = require('./splitChunk');
+const splitIntoChunks = require('./splitChunk');
+const { file } = require('googleapis/build/src/apis/file');
 
+const pdfPathForHandBook = '../../rawDataResources/2025-2026-abtech-student-handbook (1).pdf';
 
 const extractTextFromPdf = async pdfPath => {
   const dataBuffer = fs.readFileSync(pdfPath);
   const data = await pdf(dataBuffer);
 
-  return data.text;
+  return {
+    text: data.text,
+    'meta-data': data.metadata,
+  };
+};
+
+const writeToFileForTesting = async (pdfPath) => {
+    const fileName = '../../rawDataResources/testingDataPDFparse.json'
+  try {
+    const content = await extractTextFromPdf(pdfPath);
+    fs.writeFileSync(fileName, JSON.stringify(content, null, 2));
+    console.log('Success');
+  } catch (err) {
+    console.error('Unfulfilled promise for file write operation', err);
+  }
 };
 
 const returnChunksAndText = async () => {
   const pdfText = await extractTextFromPdf(
     '../../rawDataResources/2025-2026-abtech-student-handbook (1).pdf'
   );
-  const chunks = splitIntoChunks(pdfText);
+  const chunks = splitIntoChunks(pdfText.text);
 
   console.log(chunks);
 };
 
-returnChunksAndText();
+writeToFileForTesting(pdfPathForHandBook);
