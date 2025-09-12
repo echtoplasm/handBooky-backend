a1900455acfd1c6eaa2136ab42794425c5e16692
Zach <zacharyamassey@gmail.com>
2025-09-12 15:32:17 -0400

    changed pdf parsing library and wrote helper functions that return page number as well

--- utilities/pdfParse.js
+++ utilities/pdfParse.js
@@ -1,38 +1,81 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
 const splitIntoChunks = require('./splitChunk');
-const { file } = require('googleapis/build/src/apis/file');
+const PDFparser = require('pdf2json');
 
-const pdfPathForHandBook = '../../rawDataResources/2025-2026-abtech-student-handbook (1).pdf';
+const pdfp = new PDFparser();
 
-const extractTextFromPdf = async pdfPath => {
-  const dataBuffer = fs.readFileSync(pdfPath);
-  const data = await pdf(dataBuffer);
+/**
+ * @param {string} pdfPath - path to the pdf file that is being parsed
+ * @param {string} fileToExtractTo - path for the parsed chunks to be written to
+ */
+const extractHeavyMetaData = (pdfPath, fileToExtractTo) => {
+  pdfp.on('pdfParser_dataError', errData => {
+    console.error(errData.parserError);
+  });
 
-  return {
-    text: data.text,
-    'meta-data': data.metadata,
-  };
+  pdfp.on('pdfParser_dataReady', pdfData => {
+    fs.writeFileSync(`${fileToExtractTo}`, JSON.stringify(pdfData), data => console.log(data));
+    console.log(pdfData.Pages);
+  });
+
+  pdfp.loadPDF(pdfPath);
 };
 
-const writeToFileForTesting = async (pdfPath) => {
-    const fileName = '../../rawDataResources/testingDataPDFparse.json'
+/**
+ * Function that extracts text and page number from PDF for RAG
+ *
+ *  @param {string} - Path to the pdf to have page and text extracted
+ * @returns {Promise<Array<Object>>} - returns a map with page text and index
+ */
+const extractTextWithPageNo = pdfPath => {
+  return new Promise((resolve, reject) => {
+    const parser = new PDFparser();
+
+    parser.on('pdfParser_dataReady', pdfData => {
+      try {
+        const pagesWithText = pdfData.Pages.map((page, index) => {
+          const pageTexts = page.Texts.map(textObj => {
+            return decodeURIComponent(textObj.R[0].T);
+          }).join(' ');
+
+          return {
+            pageNumber: index + 1,
+            text: pageTexts,
+          };
+        });
+
+        resolve(pagesWithText);
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    parser.on('pdfParser_dataError', errData => {
+      reject(errData.parserError);
+    });
+
+    parser.loadPDF(pdfPath);
+  });
+};
+
+/**
+ * Async return function of text and page number extraction
+ * that console logs typeof and the page text to the page
+ *
+ */
+
+const processHandBook = async () => {
+  const pdfPathHandBook = '../../rawDataResources/2025-2026-abtech-student-handbook (1).pdf';
   try {
-    const content = await extractTextFromPdf(pdfPath);
-    fs.writeFileSync(fileName, JSON.stringify(content, null, 2));
-    console.log('Success');
+    const pdfWithPageNo = await extractTextWithPageNo(pdfPathHandBook);
+    console.log('Type:', typeof pdfWithPageNo);
+    console.log(pdfWithPageNo);
   } catch (err) {
-    console.error('Unfulfilled promise for file write operation', err);
+    console.error('Error processing PDF', err);
   }
 };
 
-const returnChunksAndText = async () => {
-  const pdfText = await extractTextFromPdf(
-    '../../rawDataResources/2025-2026-abtech-student-handbook (1).pdf'
-  );
-  const chunks = splitIntoChunks(pdfText.text);
+const testFileExtract = '../../rawDataResources/testingDataPDFparse1.json';
 
-  console.log(chunks);
-};
-
-writeToFileForTesting(pdfPathForHandBook);
+processHandBook();
